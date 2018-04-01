a1f97aff7d863c6d2e53f9bd5ae6042c79bb2eb5
kk2849 <37962783+kk2849@users.noreply.github.com>
2018-04-01 16:00:36 -0400

    Add files via upload

---
+++ text_io/hello_world.py
@@ -0,0 +1,14 @@
+#printing statements
+print("Hello world!\n")
+
+#expressions: +, -, *, /, **, //, %
+print("2 to the 3 power is " + str(2**3))
+print("Floor division 20//6 is " + str(20//6))
+print("20 modulus 6 is " + str(20%6))
+
+"""
+Useful modules: string, re, datetime, math, random, os, multiprocessing,
+subprocess, socket, email, json, doctest, unittest, pdb, argparse, sys
+
+Use pip in command line to install libraries
+"""
